fdd522a41f85e4ae6af1ebe88c647c79033c8286
Sarthak <sarthakdhakde@gmail.com>
2025-10-29 08:42:47 +0530

    feat: implement db schema using prisma

---
+++ server/prisma/migrations/20251029030814_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "bio" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,11 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
+
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
@@ -12,5 +14,7 @@ app.get("/health", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/user", userRoutes);
+
+export default app;
